576806101ab4cdea4d78c4a421a37ee064c350f4
Stein-N <nico-stein-98@outlook.de>
2023-11-09 15:05:16 +0100

    Adding World Generation for Arcanum Ore

--- src/main/java/net/xstopho/wizards_reborn/registries/WorldGenRegistry.java
+++ src/main/java/net/xstopho/wizards_reborn/registries/WorldGenRegistry.java
@@ -1,12 +1,16 @@
 package net.xstopho.wizards_reborn.registries;
 
 import com.mojang.serialization.Codec;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 import net.minecraft.world.gen.stateprovider.BlockStateProviderType;
 import net.minecraft.world.gen.trunk.TrunkPlacer;
@@ -23,12 +27,18 @@ public class WorldGenRegistry {
     public static RegistryKey<ConfiguredFeature<?, ?>> HUGE_MOR = registerConfiguredFeature("huge_mor");
     public static RegistryKey<ConfiguredFeature<?, ?>> HUGE_ELDER_MOR = registerConfiguredFeature("huge_elder_mor");
 
+    public static RegistryKey<PlacedFeature> ARCANUM_ORE = registerPlacedFeature("arcanum_ore");
+
     public static final BlockStateProviderType<SupplierBlockStateProvider> AN_STATEPROVIDER = registerBlockStateProviderType("an_stateprovider", SupplierBlockStateProvider.CODEC);
 
     public static final TrunkPlacerType<ArcaneWoodTrunkPlacer> ARCANE_WOOD_TRUNK_PLACER = registerTrunkPlacer("arcane_wood_trunk_placer", ArcaneWoodTrunkPlacer.CODEC);
 
     private static RegistryKey<ConfiguredFeature<?, ?>> registerConfiguredFeature(String id) {
         return RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, new Identifier(WizardsReborn.MOD_ID, id));
+    }
+
+    private static RegistryKey<PlacedFeature> registerPlacedFeature(String id) {
+        return RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(WizardsReborn.MOD_ID, id));
     }
 
     private static <P extends TrunkPlacer> TrunkPlacerType<P> registerTrunkPlacer(String id, Codec<P> codec) {
@@ -40,6 +50,6 @@ public class WorldGenRegistry {
     }
 
     public static void init() {
-
+        BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, ARCANUM_ORE);
     }
 }
